4f65f2a3bf4c69bfb7b2fb347c40a1e16e683d31
roysreejan <sreejanroy01@gmail.com>
2025-07-08 13:28:53 +0600

    img url updated

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['localhost'],
+    domains: ['example.com', 'www.aazbd.com', 'xcdn.next.co.uk'],
   },
 };
 
